d23bc3d1d42f97b63e05d37ad58cd3ff75ff6640
João Vitor Menezes de Jesus <joao_vitor_mj@hotmail.com>
2022-05-04 22:52:34 -0300

    feat-save survey -c 2:12

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -186,12 +186,13 @@
 import { v4 as uuidv4 } from "uuid";
 import store from "../store";
 import { ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 import PageComponent from "../components/PageComponent.vue";
 import QuestionEditor from "../components/editor/QuestionEditor.vue";
 
 const route = useRoute();
+const router = useRouter();
 
 //Create empty survey
 let model = ref({
@@ -232,6 +233,16 @@ function questionChange(question) {
       return JSON.parse(JSON.stringify(question));
     }
     return q;
+  });
+}
+
+//Create or update survey
+function saveSurvey() {
+  store.dispatch("saveSurvey", model.value).then(({ data }) => {
+    router.push({
+      name: "SurveyView",
+      params: { id: data.data.id },
+    });
   });
 }
 </script>
